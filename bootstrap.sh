6039cedc0ba8ca1e5cc7bd3d657634f5961b8bea
w4solutions <waristonfernando@gmail.com>
2017-07-12 17:55:15 -0300

    fix all licenses

--- bootstrap.sh
+++ bootstrap.sh
@@ -35,9 +35,15 @@ expect {
 
 sudo wget --quiet --output-document=tools.zip https://dl.google.com/android/repository/sdk-tools-linux-3859397.zip
 sudo unzip -qq tools.zip -d ~/android-sdk-linux
-sudo echo y | ~/android-sdk-linux/tools/bin/sdkmanager "platforms;android-22" --sdk_root=~/android-sdk-linux
-sudo echo y | ~/android-sdk-linux/tools/bin/sdkmanager "platform-tools" --sdk_root=~/android-sdk-linux
-sudo echo y | ~/android-sdk-linux/tools/bin/sdkmanager "build-tools;22.0.1" --sdk_root=~/android-sdk-linux
+
+expect -c '
+set timeout -1   ;
+spawn /home/ubuntu/android-sdk-linux/tools/bin/sdkmanager --licenses --sdk_root=/home/ubuntu/android-sdk-linux/ 
+expect {
+    "ccepted" { exp_send "y\r" ; exp_continue }
+    eof
+}
+'
 
 sudo apt-get -y install lib32stdc++6 lib32z1
 
