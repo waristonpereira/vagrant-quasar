aa28fd82622e28be094db598d1983b50dc5d9ee5
waristonpereira <waristonfernando@gmail.com>
2017-08-29 10:24:12 -0300

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,13 +3,13 @@
 ANDROID_SDK_FILENAME=android-sdk_r23.0.1-linux.tgz
 ANDROID_SDK=http://dl.google.com/android/$ANDROID_SDK_FILENAME
 
-sudo apt-get update
-sudo apt-get install -y openjdk-8-jdk openjdk-8-jre lib32stdc++6 lib32z1 expect unzip
-sudo curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
-sudo apt-get install -y nodejs
-sudo npm install -g ionic cordova
+apt-get update
+apt-get install -y openjdk-8-jdk openjdk-8-jre lib32stdc++6 lib32z1 expect unzip
+curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
+apt-get install -y nodejs
+npm install -g ionic cordova
 curl -O $ANDROID_SDK
-tar -xzf $ANDROID_SDK_FILENAME -C ~/
+tar -xzf $ANDROID_SDK_FILENAME -C /home/ubuntu/
 sudo npm config set bin-links false -g
 
 sudo mkdir /opt/gradle
@@ -20,11 +20,11 @@ echo "PATH=\$PATH:/opt/gradle/gradle-3.4.1/bin" >> /home/ubuntu/.bashrc
 echo "ANDROID_HOME=~/android-sdk-linux" >> /home/ubuntu/.bashrc
 echo "PATH=\$PATH:~/android-sdk-linux/tools:~/android-sdk-linux/platform-tools" >> /home/ubuntu/.bashrc
 
-mkdir ~/.android/
-touch ~/.android/repositories.cfg
+mkdir /home/ubuntu/.android/
+touch /home/ubuntu/.android/repositories.cfg
 
 wget --quiet --output-document=tools.zip https://dl.google.com/android/repository/sdk-tools-linux-3859397.zip
-unzip -oqq tools.zip -d ~/android-sdk-linux
+unzip -oqq tools.zip -d /home/ubuntu/android-sdk-linux
 
 expect -c '
 set timeout -1   ;
